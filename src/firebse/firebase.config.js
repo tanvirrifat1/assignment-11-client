30fdf22c5252d0c032402866a332873c2609e96a
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2022-11-07 22:10:54 +0600

    footer added

---
+++ src/firebse/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCsA5eC6rrHX94Zp9U1OkBF-Zr9dG8KS1s",
+    authDomain: "woven-parsec-367104.firebaseapp.com",
+    projectId: "woven-parsec-367104",
+    storageBucket: "woven-parsec-367104.appspot.com",
+    messagingSenderId: "28905365930",
+    appId: "1:28905365930:web:13eac72e4c798b78950bd3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app
